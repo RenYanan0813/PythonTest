0e252672d1d0e45b12697a9d5ea8096634318adf
RenYaNan <renyn813@qq.com>
2018-04-02 14:59:38 +0800

    digui

---
+++ digui.py
@@ -0,0 +1,12 @@
+#-*- coding: utf-8 -*-
+
+def diGui(x):
+	#if x == 1:
+		#return 1
+	n = 0    # n=1 ，0 为什么不一样呢？
+	if x > 0:
+		n = x * diGui(x-1) 
+	return n
+
+if __name__ == '__main__':
+	print(diGui(5))
